8c53f79410e2e34c375c10ef1d7c2e1929a9acf0
Draldo <draldo_lopez@hotmail.com>
2016-07-28 17:32:31 -0400

    Broadcast and interface passing data

---
+++ app/src/main/java/com/example/fragmentcommunication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.fragmentcommunication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+//2. Implement Interface
+public class MainActivity extends AppCompatActivity implements BlankFragment.ActivityInterface {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    public static final String CUSTOM_EVENT_KEY = "CUSTOM_ACTION_BROADCAST";
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "doMagic: BroadcastReceiver " + intent.getStringExtra("KEY"));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LocalBroadcastManager
+                .getInstance(this)
+                .registerReceiver(mMessageReceiver,
+                        new IntentFilter(CUSTOM_EVENT_KEY));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager
+                .getInstance(this)
+                .unregisterReceiver(mMessageReceiver);
+    }
+
+    @Override
+    public void doMagic(String name) {
+        Log.d(TAG, "doMagic: Interface " + name);
+    }
+}
